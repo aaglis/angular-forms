c9d6c856edc7a53382177b1a7d122368b54f0a85
aaglis <aglissilva17@gmail.com>
2024-06-21 17:11:45 -0300

    feat: template-driven-form page

--- src/app/pages/template-driven-form-page/template-driven-form-page.component.ts
+++ src/app/pages/template-driven-form-page/template-driven-form-page.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-template-driven-form-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './template-driven-form-page.component.html',
   styleUrl: './template-driven-form-page.component.scss'
 })
 export class TemplateDrivenFormPageComponent {
+  formSended = false
+  arrayTechs = signal<string[]>(['Angular', 'React', 'Vue', 'NextJs'])
 
+  user = {
+    name: '',
+    email: '',
+    password: '',
+    gender: '',
+    mainTech: ''
+  }
+
+  submitForm() {
+    this.formSended = true
+    console.log(this.user)
+  }
+  
 }
